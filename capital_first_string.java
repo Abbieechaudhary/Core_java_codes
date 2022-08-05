777a9de452603ec61a55d40d1ad03beaad7e6aed
Akash Chaudhary <79572198+Abbieechaudhary@users.noreply.github.com>
2022-08-05 20:44:55 +0530

    Update capital_first_string.java

--- capital_first_string.java
+++ capital_first_string.java
@@ -1,22 +1,23 @@
 package com.company;
 
 public class capital_first_string {
-    public static void input(String str){
-        str=" "+str;
-        String s1="";
-        for(int i=0;i<str.length();i++)
+    public static String input(String str){
+        str=" "+str;            //make a space before the first word of the string
+        String s1="";           //create a string to make output string
+
+        for(int i=0;i<str.length();i++)  //loop for run through index
         {
-            if(str.charAt(i)==' ')
+            if(str.charAt(i)==' ')  // check space available
             {
-                   s1=s1+Character.toUpperCase(str.charAt(i+1))+".";
+                s1=s1+Character.toUpperCase(str.charAt(i+1))+".";   //store character at index2(except space) and add dot . in string(s1)
             }
         }
-        System.out.println(s1);
+        return s1;            //return string s1
     }
 
     public static void main(String[] args) {
         String str="my name is aakash chaudhary";
-        input(str);
-        System.out.println();
+        System.out.println(input(str));
     }
 }
+
