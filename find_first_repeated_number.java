1c964d2c35ccd20cadc645260534e3040fb9a7d0
Akash Chaudhary <79572198+Abbieechaudhary@users.noreply.github.com>
2022-08-30 16:51:25 +0530

    Add files via upload

---
+++ find_first_repeated_number.java
@@ -0,0 +1,38 @@
+class find_first_repeated_number {
+    public int find_first(int[] num) {
+//        int min=-1;
+//        HashSet <Integer> set=new HashSet<>();
+//        for(int i=0;i<num.length;i++){
+//            if (set.contains(num[i])){
+//                min=i;
+//                break;
+//            }
+//            else{
+//                set.add(num[i]);
+//            }
+//        }
+//        return num[min];
+        int []arr=new int[6];
+        int min=-1;
+        for(int i=0;i< num.length;i++){
+                if (num[i] == arr[i]) {
+                    min = i;
+                    break;
+                } else {
+                    arr = num;
+                }
+            }
+        return num[min];
+    }
+
+    public static void main(String[] args) {
+        int input1[]={1,2,4,1,2,8};
+        int input2[]={1,2,3,3,2,1};
+        find_first_repeated_number ob=new find_first_repeated_number();
+        int r1=ob.find_first(input1);
+        int r2=ob.find_first(input2);
+        System.out.println("output1 = "+r1);
+        System.out.println("output2 = "+r2);
+
+    }
+}
